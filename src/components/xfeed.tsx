196f0ba37ed8784f08550219fb3b2d50d0a39396
mikianaa <mikia.peace@gmail.com>
2025-03-12 15:54:16 +0900

    xのフィードが表示されないバグを解消

--- src/components/xfeed.tsx
+++ src/components/xfeed.tsx
@@ -1,19 +1,29 @@
-const XFeed = () => (
-  <div className="twitter-feed">
-    <a
-      className="twitter-timeline"
-      data-width="400"
-      data-height="500"
-      href="https://twitter.com/PXIf8kVZCLOlrrD?ref_src=twsrc%5Etfw"
-    >
-      Tweets by Mikirin!!!
-      <script
+import { useEffect } from "react";
+
+const XFeed = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.setAttribute("src", "https://platform.twitter.com/widgets.js");
+    script.setAttribute("async", "true");
+    document.body.appendChild(script);
+  }, []);
+  return (
+    <div className="twitter-feed">
+      <a
+        className="twitter-timeline"
+        data-width="400"
+        data-height="500"
+        href="https://twitter.com/PXIf8kVZCLOlrrD?ref_src=twsrc%5Etfw"
+      >
+        Tweets by Mikirin!!!
+        {/* <script
         async
         src="https://platform.twitter.com/widgets.js"
         charSet="utf-8"
-      ></script>
-    </a>
-  </div>
-);
+      ></script> */}
+      </a>
+    </div>
+  )
+};
 
 export default XFeed;
