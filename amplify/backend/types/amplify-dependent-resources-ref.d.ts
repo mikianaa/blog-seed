aabf268d9fd6ac095f69a49ce600cf26f96a2cd1
mikianaa <mikia.peace@gmail.com>
2025-05-08 15:24:16 +0900

    Amplifyの設定を更新 Authの設定を削除

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,15 +1,4 @@
 export type AmplifyDependentResourcesAttributes = {
-  "auth": {
-    "blogseede70db8fa": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
-  },
   "storage": {
     "mikiaSeedAmplifyStorage": {
       "BucketName": "string",
