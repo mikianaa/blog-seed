74830192cc5e1b8e66b5fc29e300f12a0d4dffbd
mikianaa <mikia.peace@gmail.com>
2025-05-08 18:45:35 +0900

    Amplifyの設定を更新 Authを再度追加

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,15 @@
 export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "blogseedeb42ed36": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
   "storage": {
     "mikiaSeedAmplifyStorage": {
       "BucketName": "string",
