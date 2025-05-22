79b246c2f5a0495fda33e6be74ce94419ca2e10e
mikianaa <mikia.peace@gmail.com>
2025-05-22 12:03:15 +0900

    ヘッダーの不要な部分を削除

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,11 +3,6 @@ import { useRouter } from "next/router";
 
 const Header = () => {
   const router = useRouter();
-  const currentPath = router.pathname;
-
-  const handleSignOut = async () => {
-    router.push("/")
-  };
 
   return (
     <header className="sticky top-0 border-b h-20 z-10 bg-white">
@@ -17,19 +12,6 @@ const Header = () => {
             Seed
           </div>
         </Link>
-        {/* <div className="mb-4">
-          {currentPath === "/new-post" ? (
-            <button className="button" type="button" onClick={handleSignOut}>
-              Back to Top
-            </button>
-          ) : (
-            <Link href="/new-post">
-              <div className="bg-blue-500 text-white px-4 py-2 rounded">
-                New Post
-              </div>
-            </Link>
-          )}
-        </div> */}
       </div>
     </header>
   );
