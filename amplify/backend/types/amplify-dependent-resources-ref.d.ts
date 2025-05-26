30c9fc90f2e8d3b4a88849f7adc3dc0a0d7f51dd
mikianaa <mikia.peace@gmail.com>
2025-05-26 17:52:25 +0900

    amplifyフォルダを再設定

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,21 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "blogseed411be2b7": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s396cc7ffd": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
